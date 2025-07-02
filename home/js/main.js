1bb98e9c659753fbafae5cde3040c8a1f92840f6
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2025-07-02 21:58:56 +0800

    Update main.js

--- home/js/main.js
+++ home/js/main.js
@@ -3,6 +3,13 @@ if (location.host !== "sysri.cn") {
     location.replace(`https://sysri.cn${pathname}${search}${hash}`);
 }
 
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=
+p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"mxana.tacool.com/zdz.js"),n=e.createElement(\"script\"),
+r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribut
+e(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"MXA_COLLECT\",i.d=n;var o=function(){s.MX
+A&&s.MXA.init&&s.MXA.init(i)};n.onload=function(){o()},r.parentNode.insertBefore(n,r)}()}({ id: "c1-DcvmmKgc" });</script>
+");
+
 
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
